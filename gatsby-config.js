5ccbdf7ba27196b5a73614149196c69167de5ed1
Brian Perry <contact.brianperry@gmail.com>
2019-11-17 22:10:19 -0600

    Installed gatsby-theme-drupal-markdown-post

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,33 +7,11 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-source-drupal`,
+      resolve: "@backlineint/gatsby-theme-drupal-markdown-post",
       options: {
-        baseUrl: `http://brian-perry-interactive.lndo.site:8000/`
-      }
+        baseUrl: "http://brian-perry-interactive.lndo.site:8000/",
+        basePath: "/posts",
+      },
     },
-    `gatsby-plugin-sharp`, 
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `@weknow/gatsby-remark-drupal`,
-            options: {
-              nodes: [`gatsby_md_post_markdown_post`]
-            }
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 1080,
-            },
-          },
-        ]
-      }
-    }
-  ]
+  ],
 }
